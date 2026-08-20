b37a577fb73ad48b2244a88383ae9a53d7e4c82d
amrayman-web <amr_ayman011@yahoo.com>
2026-08-20 21:13:18 +0300

    upload JS task 1 + CSS project

---
+++ Project_3/JS task/script.js
@@ -0,0 +1,113 @@
+
+// 1-declare student name
+
+let studentName = "Amr Ayman";
+
+console.log(studentName);
+
+// 2- declare two numbers and add them
+
+let a = 40;
+let b = 50;
+
+console.log(a + b);
+
+// 3. Declare a variable without assigning a value and print it
+
+let x;
+
+console.log(x);
+
+// 4. Declare a constant country and print it. 
+
+const country = "Egypt";
+
+console.log(country)
+
+// 5. Declare three variables using var, let, and const and print them.
+
+var firstName = "Amr";
+let age = 28;
+const city = "Cairo";
+
+console.log(firstName);
+console.log(age);
+console.log(city);
+
+// 6. Create a number variable and print its value and type. 
+
+let number = 76;
+
+console.log(number);
+
+// 7. Create a string variable and print its value and type.
+
+let message = "Hello World";
+
+console.log(message);
+
+
+// 8. Create a boolean variable and print its value and type.
+
+let isActive = true;
+
+console.log(isActive);
+
+
+// 9. Declare a variable and assign undefined. Print its type.
+
+let undefinedVariable = undefined;
+
+
+// 10. Declare a variable with null and print its type.
+
+let nullVariable = null;
+
+// 11. Find the remainder using %.
+
+let y = 25;
+let z = 6;
+console.log(y % z);
+
+// 12. Use increment and decrement operators and print results.
+
+let f = 10;
+f++;
+console.log(f);
+
+f--;
+console.log(f);
+
+// 13. Calculate the total cost of 3 items using variables.
+let basketBall = 150;
+let volleyBall = 200;
+let footBall = 500;
+
+let total = basketBall + volleyBall + footBall;
+console.log(total);
+
+// 14. Calculate the average of 3 numbers.
+let number1 = 90;
+let number2 = 210;
+let number3 = 300;
+
+let average = (number1 + number2 + number3) / 3;
+console.log(average);
+
+// 15. Increase a number by 10 using assignment operators.
+
+let color = 65;
+color += 10;
+console.log(color);
+
+// 16. Decrease a number by 5 using assignment operators.
+let player = 80;
+player -= 5;
+console.log(player);
+
+// 17. Multiply a number by 3 using assignment operators.
+
+let number4 = 10;
+number4 *= 3;
+console.log(number4);
+
